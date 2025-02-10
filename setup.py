49c2357d51398599a90bb01a26419154e538bfdb
cwilliams001 <williamsct1@gmail.com>
2025-02-10 16:17:24 -0500

    fixed build issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="channel-surfer",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "rich",
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "channel-surfer=channel_surfer:main",
+            "channel-surfer=channel_surfer.main:main",
         ],
     },
     author="CWilliams",
